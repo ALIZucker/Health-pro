4f402b2109d094d2e0ce9aedf29d5608b9a21b07
ALI_Zucker <zabihiali1998@gmail.com>
2023-11-25 16:48:48 +0330

    epoch|(3)

--- assets/js/health.js
+++ assets/js/health.js
@@ -2,12 +2,12 @@ let height = document.getElementById('height-input')
 let weight = document.getElementById('weght-input')
 let rangebox = document.getElementById('show-reasult')
 let range = document.getElementById('vol')
-let box1 = document.getElementById('sectionone')
-let box2 = document.getElementById('input-bmi')
+let show = document.getElementById('lable-show')
+
 
 function checkvalue(text) {
-    if (text.length == 0) {
-        alert('Enter number again');
+    if (0 > text || text > 300) {
+        alert('Enter number  again');
         return false;
     } else {
         return true;
@@ -16,7 +16,7 @@ function checkvalue(text) {
 }
 
 function BMI() {
-    if (checkvalue(height.value) && checkvalue(weight.value)) {
+    if (checkvalue((parseInt(height.value))) && checkvalue(parseInt(weight.value))) {
         bmi = parseInt(weight.value) / ((parseInt(height.value) / 100) * (parseInt(height.value) / 100))
         showreasult(bmi)
     }
@@ -24,7 +24,8 @@ function BMI() {
 
 function showreasult(bmi) {
 
-    rangebox.style.display = "block"
+    rangebox.style.display = "inline-block"
     range.value = bmi
+    show.innerText = bmi
 
 }
